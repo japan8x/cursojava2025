d9fcd4116f2bc03d09fc5b85497123fafde847ff
Tardes <Tardes@GET-03>
2025-07-07 19:57:00 +0200

    variable entero

--- src/es/cursojava/inicio/bucles/BucleFor.java
+++ src/es/cursojava/inicio/bucles/BucleFor.java
@@ -4,11 +4,11 @@ public class BucleFor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int a = 0;
 		for( ; ; ) {
 			
 		}
-		
+		// 
 		for (int i=0;i<5;i++) {
 			System.out.println();
 		}
