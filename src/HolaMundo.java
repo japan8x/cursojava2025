0b367736170c4916029190b1ea15c266b6f4acf7
Tardes <Tardes@GET-03>
2025-07-08 17:41:12 +0200

    Repositorio Local

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	
 	public static void main (String [] args ) {
 		
-		System.out.println("Hola Getafe Juan Alberto 9");
+		System.out.println("Hola Getafe Juan Alberto 10");
 		
 		// Comentario de línea
 		
@@ -21,7 +21,8 @@ public class HolaMundo {
 		
 		int edad = 25; //Declarar e inicializar
 		
-		// syso
+		System.out.println("Repositorio Local");
+		
 		
 		System.out.println("10" + numero + edad);
 		System.out.println("resultado: " + (numero + edad) );
