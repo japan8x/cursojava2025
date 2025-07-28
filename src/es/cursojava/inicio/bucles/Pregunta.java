783d27264315080478993d5018c3e90ea8c74b83
Tardes <Tardes@GET-03>
2025-07-28 20:15:59 +0200

    L28072025 test

---
+++ src/es/cursojava/inicio/bucles/Pregunta.java
@@ -0,0 +1,16 @@
+package es.cursojava.inicio.bucles;
+
+public class Pregunta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 2; 
+		for(int i=0, j=1;j<numero+i;i++,j++) {
+			
+			System.out.println(i);
+			
+		}
+	}
+
+}
