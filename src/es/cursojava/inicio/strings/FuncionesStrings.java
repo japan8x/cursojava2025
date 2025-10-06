a6b3e1e5c446f4b1235dd234efd7922d3dd01b8c
Tardes <Tardes@GET-03>
2025-10-06 17:43:46 +0200

    L06102025

---
+++ src/es/cursojava/inicio/strings/FuncionesStrings.java
@@ -0,0 +1,17 @@
+package es.cursojava.inicio.strings;
+
+public class FuncionesStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombre1 = "Marta";
+		String nombre2 = "Eva";
+		String nombre3 = new String();
+		String nombre4 = null;
+		
+		System.out.println(nombre4.toUpperCase());
+		
+	}
+
+}
